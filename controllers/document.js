222e2bbbb6d8c6322055590ad058b4d34e98c28b
Akash Raj <ebookexer@gmail.com>
2025-08-03 20:20:40 +0530

    Implement user authentication and management features with Express, including login and user registration routes, and integrate MongoDB for data storage. Add middleware for error handling and token extraction, and update app configuration to include new routes and dependencies.

--- controllers/document.js
+++ controllers/document.js
@@ -1,12 +1,29 @@
 import { Router } from 'express';
 
+import Chunk from '../models/chunk.js';
+import Document from '../models/document.js';
+import User from '../models/user.js';
+import { createEmbeddings } from '../services/createEmbeddings.js';
 import handlePDFProcessing from '../services/processPdf.js';
 import { error } from '../utils/logger.js';
 
 const documentRouter = Router();
 documentRouter.post('/hackrx/run', async (req, res) => {
   try {
+    const user = await User.findById(req.user);
+    if (!user) {
+      return res.status(404).json({ error: 'userId missing or not valid' });
+    }
+
     const { documentURL, questions } = req.body;
+
+    let document = await Document.findOne({
+      originalUrl: documentURL,
+      user: user._id,
+    });
+    if (!document) {
+      document = new Document({ originalUrl: documentURL, user: user._id });
+    }
 
     if (!documentURL) {
       return res.status(400).json({ error: 'documentURL is required' });
